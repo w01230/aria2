7d68ccfe06294e05e351e9300a8c709d4e4931a9
w01230 <4585006@gmail.com>
2018-01-11 20:25:19 +0800

    update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -4,6 +4,6 @@ sudo docker run -d \
 		--restart=always \
 		--name=aria2 \
 		-p 6800:6800 \
-		-e "SECRET=xxxxxx" \
-		-v /home/jome/download:/data \
-		aria2
+		-e "SECRET=secret" \
+		-v /nas/download/TDDOWNLOAD:/data \
+		aria2:1.0
--- monitor.sh
+++ monitor.sh
@@ -5,26 +5,26 @@ list=`wget -qO-  https://github.com/ngosang/trackerslist/raw/master/trackers_all
 
 if [ -z `grep "bt-tracker" ${config}` ]; then
     sed -i '$a bt-tracker='${list} ${config} 
-    echo add...done.
+    echo add bt-tracker done.
 else
     sed -i "s@bt-tracker.*@bt-tracker=${list}@g" ${config} 
-    echo update...done.
+    echo update bt-tracker done.
 fi
 
 if [ -z `grep "rpc-secret" ${config}` ]; then
     sed -i '$a rpc-secret='${SECRET} ${config} 
-    echo add...done.
+    echo add rpc-secret done.
 else
     sed -i "s@rpc-secret.*@rpc-secret=${SECRET}@g" ${config} 
-    echo update...done.
+    echo update rpc-secret done.
 fi
 
 while true; 
 do
     pid=`ps -ef|grep aria2c|grep -v grep|awk '{print $2}'`
     if [ -z "$pid" ]; then
-        echo "restarting..."
         /usr/bin/aria2c --conf-path=/etc/aria2.conf
+	echo "start aria2c done."
     else
         sleep 30
     fi
