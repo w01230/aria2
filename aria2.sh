667dd0cf6ca10cc9c74f3f800c7e4621a1567b71
w01230 <4585006@gmail.com>
2018-02-07 21:21:05 +0800

    fix aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -2,9 +2,9 @@
 
 docker run -d \
 	--restart=always \
-	--name=aria2c \
-	-p 6801:6800 \
+	--name=aria2 \
+	-p 6800:6800 \
 	-p 6882:6882 \
 	-e "SECRET=0x000000" \
 	-v /nas/download/TDDOWNLOAD:/data \
-	aria2
+	aria2:1.0
